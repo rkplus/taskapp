dbf375b718cca13cbb5bd369b3798943c64b73fb
Ratnesh Kumar <Ratnesh@Ratnesh-no-MacBook-Air.local>
2017-09-29 09:00:21 +0900

    taskapp作成

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by Ratnesh Kumar on 2017/09/28.
+//  Copyright © 2017年 ratonesshu_kuma_ru. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
